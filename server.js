972de51eb3c04cbe3e0e4d091c39af55dadab140
youkyunghee378-tech <youkyunghee378@gmail.com>
2026-03-23 08:01:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,25 @@ const http = require('http');
 
 const port = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end('Hello from Jenkins + Azure 🚀');
-}).listen(port);
+const server = http.createServer((req, res) => {
+
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello from Jenkins + Azure 🚀');
+  }
+
+  else if (req.url === '/api/health') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ status: 'ok' }));
+  }
+
+  else {
+    res.writeHead(404);
+    res.end('Not Found');
+  }
+
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
